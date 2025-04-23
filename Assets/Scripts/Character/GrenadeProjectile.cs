6fc4b687a008fca3e2b2e9274d0f2f8ec6faa324
warreR33 <154948249+warreR33@users.noreply.github.com>
2025-04-23 15:27:52 -0300

    Armas
    Tenemos 2 armas
    -Pistola
    -Lazagrandas
    
    Se activan cn el 1 y el 2. El lanzagranadas tiene un CD alto para que no se haga spam

---
+++ Assets/Scripts/Character/GrenadeProjectile.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GrenadeProjectile : MonoBehaviourPun
+{
+    public float explosionRadius = 5f;
+    public int explosionDamage = 50;
+    public float lifeTime = 3f;
+    public GameObject explosionEffect;
+
+    private int attackerViewID;
+    private Rigidbody rb;
+    private bool hasExploded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(ExplodeAfterDelay());
+    }
+
+    public void SetAttacker(int viewID)
+    {
+        attackerViewID = viewID;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (hasExploded) return;
+
+        IDamageable target = collision.collider.GetComponent<IDamageable>();
+        if (target != null)
+        {
+            Explode(); 
+        }
+        else
+        {
+            // Rebote automático gracias al Rigidbody + Physics Material
+            // Podés ajustar el bounciness/friction en el material del collider
+        }
+    }
+
+    IEnumerator ExplodeAfterDelay()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        if (!hasExploded)
+            Explode();
+    }
+
+    void Explode()
+    {
+        hasExploded = true;
+
+        if (explosionEffect != null)
+        {
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        }
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+            foreach (Collider col in colliders)
+            {
+                IDamageable damageable = col.GetComponent<IDamageable>();
+                if (damageable != null)
+                {
+                    damageable.TakeDamage(explosionDamage,attackerViewID); 
+                }
+            }
+        }
+
+        PhotonNetwork.Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,27 +7,62 @@ using UnityEngine.UI;
 public class Shooter : MonoBehaviour
 {
     public GameObject projectilePrefab;
+    public GameObject grenadePrefab;
+
+    public GameObject pistolModel;
+    public GameObject grenadeLauncherModel;
+
     public Transform shootPoint;
-    public float fireRate = 0.5f;
     public Camera playerCamera;
     public Image crosshairImage;
+
+    public float pistolFireRate = 0.5f;
+    public float grenadeCooldown = 20f;
+
+    private float lastPistolShotTime = -999f;
+    private float lastGrenadeShotTime = -999f;
+
 
     private PhotonView photonView;
 
 
     private float lastShotTime;
 
-    private void Start()
+    private enum WeaponType { Pistol, GrenadeLauncher }
+    private WeaponType currentWeapon = WeaponType.Pistol;
+
+     private void Start()
     {
         photonView = GetComponent<PhotonView>();
-
+        EquipWeapon(WeaponType.Pistol);
     }
 
     void Update()
     {
         if (!photonView.IsMine) return;
 
-        //Cambiar color de la mira si apunta a enemigo
+        HandleWeaponSwitch();
+        HandleCrosshair();
+        HandleShooting();
+    }
+
+    void HandleWeaponSwitch()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            EquipWeapon(WeaponType.Pistol);
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            EquipWeapon(WeaponType.GrenadeLauncher);
+    }
+
+    void EquipWeapon(WeaponType weapon)
+    {
+        currentWeapon = weapon;
+        pistolModel.SetActive(weapon == WeaponType.Pistol);
+        grenadeLauncherModel.SetActive(weapon == WeaponType.GrenadeLauncher);
+    }
+
+    void HandleCrosshair()
+    {
         Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         if (Physics.Raycast(ray, out RaycastHit hit, 30f))
         {
@@ -36,27 +71,47 @@ public class Shooter : MonoBehaviour
             else if (crosshairImage)
                 crosshairImage.color = Color.white;
         }
+    }
 
-        //Disparo
-        if (Input.GetMouseButton(0) && Time.time - lastShotTime > fireRate)
+    void HandleShooting()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Vector3 targetPoint;
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 30f))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.origin + ray.direction * 30f;
+
+        Vector3 direction = (targetPoint - shootPoint.position).normalized;
+
+        if (currentWeapon == WeaponType.Pistol)
         {
-            lastShotTime = Time.time;
-
-            Vector3 targetPoint;
-
-            if (hit.collider != null)
+            if (Time.time - lastPistolShotTime >= pistolFireRate)
             {
-                targetPoint = hit.point;
+                lastPistolShotTime = Time.time;
+
+                GameObject projectile = PhotonNetwork.Instantiate(projectilePrefab.name, shootPoint.position, Quaternion.LookRotation(direction));
+                projectile.GetComponent<Projectile>().SetAttacker(photonView.ViewID);
+            }
+        }
+        else if (currentWeapon == WeaponType.GrenadeLauncher)
+        {
+            if (Time.time - lastGrenadeShotTime >= grenadeCooldown)
+            {
+                lastGrenadeShotTime = Time.time;
+
+                GameObject grenade = PhotonNetwork.Instantiate(grenadePrefab.name, shootPoint.position, Quaternion.identity);
+                grenade.GetComponent<Rigidbody>().velocity = direction * 15f;
+                grenade.GetComponent<GrenadeProjectile>().SetAttacker(photonView.ViewID);
             }
             else
             {
-                targetPoint = ray.origin + ray.direction * 30f;
+                float timeLeft = grenadeCooldown - (Time.time - lastGrenadeShotTime);
+                Debug.Log($"Lanzagranadas en cooldown: {timeLeft:F1}s restantes");
             }
-
-            Vector3 direction = (targetPoint - shootPoint.position).normalized;
-
-            GameObject projectile = PhotonNetwork.Instantiate(projectilePrefab.name, shootPoint.position, Quaternion.LookRotation(direction));
-            projectile.GetComponent<Projectile>().SetAttacker(photonView.ViewID);
         }
     }
 
